65220fed9b8361f7a53a3960b63b53e5017b5c17
jbialon32 <35343963+jbialon32@users.noreply.github.com>
2019-06-29 15:41:14 -0500

    Add files via upload
    Update CreateUser and made MessageCreate

--- server-side/php/CreateUser.php
+++ server-side/php/CreateUser.php
@@ -5,45 +5,48 @@ $response = array();
 //Check for mandatory parameters
 if(isset($_POST["email"])&&isset($_POST["username"])&&isset($_POST["password"])&&isset($_POST["fname"])&&isset($_POST["lname"])){
   $db = connectSQL();
-	$email = $_POST["email"];
-	$username = $_POST["username"];
+	$email = strToUpper($_POST["email"]);
+	$username = strToUpper($_POST["username"]);
 	$pass = $_POST["password"];
   $fname = $_POST["fname"];
-  $lname = $_POST["lname"];
-	
-	//Query to insert a user
-	$query = "INSERT INTO users(email, user_name, pass) VALUES (?,?,?)";
-	//Prepare the query
-	if($stmt = $db->prepare($query)){
-		//Bind parameters
-		$stmt->bind_param("sss",$email,$username,$pass);
-		//Exceting MySQL statement
-		$stmt->execute();
-		//Check if data got inserted
-		if($stmt->affected_rows == 1){
-      $response = getUserByEmail($db, $email);
-      if($response["success"] = 1){
-        $userID = $response["id"];
-        $response = addUserInfo($db, $userID, $fname, $lname);
-        if($response["success"] = 1){
-          $response = addUserRole($db, $userID);
-          if($response["success"] = 1){
-            $response = addUserToWelcome($db, $userID);
-          }
-        }
-      }
-			
+	$lname = $_POST["lname"];
+	$response = uniqueValidation($db, $username, $email);
+
+	if($response["success"] == 1){
+		//Query to insert a user
+		$query = "INSERT INTO users(email, user_name, pass) VALUES (?,?,?)";
+		//Prepare the query
+		if($stmt = $db->prepare($query)){
+			//Bind parameters
+			$stmt->bind_param("sss",$email,$username,$pass);
+			//Exceting MySQL statement
+			$stmt->execute();
+			//Check if data got inserted
+			if($stmt->affected_rows == 1){
+				$response = getUserByEmail($db, $email);
+				if($response["success"] = 1){
+					$userID = $response["id"];
+					$response = addUserInfo($db, $userID, $fname, $lname);
+					if($response["success"] = 1){
+						$response = addUserRole($db, $userID);
+						if($response["success"] = 1){
+							$response = addUserToWelcome($db, $userID);
+						}
+					}
+				}
+			}else{
+				//Some error while inserting
+				$response["success"] = 0;
+				$response["message"] = "Error while adding user";
+			}					
 		}else{
 			//Some error while inserting
 			$response["success"] = 0;
-			$response["message"] = "Error while adding user";
-		}					
-	}else{
-		//Some error while inserting
-		$response["success"] = 0;
-		$response["message"] = mysqli_error($db);
+			$response["message"] = mysqli_error($db);
+		}
+
+		$db->close();
 	}
- 
 }else{
 	//Mandatory parameters are missing
 	$response["success"] = 0;
@@ -76,6 +79,8 @@ function getUserByEmail($db, $email){
       $response["success"] = 0;
       $response["message"] = "ID retrieval Failed.";
     }
+
+    $db->close();
 
   }else{
     //When some error occurs
@@ -174,6 +179,47 @@ function addUserToWelcome($db, $userID){
   }
   
   return $response;
+
+}
+
+function uniqueValidation($db, $username, $email){
+	$user = "";
+	$mail = "";
+	$query = "SELECT user_name, email FROM users WHERE user_name=? OR email=?";
+	if($stmt = $db->prepare($query)){
+		//Bind user_name parameter to the query
+		$stmt->bind_param("ss", $username, $email);
+		$stmt->execute();
+		//Bind fetched result to variables $userID, $pass
+		$stmt->bind_result($user, $mail);
+		$user = strToUpper($user);
+		$mail = strToUpper($mail);
+		//Check for results		
+		if($stmt->fetch()){
+			//Check user entry with stored password
+			if(strcmp($username, $user) == 0){
+				$response["success"] = 0;
+				$response["message"] = "Username Already Taken.";
+			}
+
+			if(strcmp($email, $mail) == 0){
+				$response["success"] = 0;
+				$response["message"] = "Username Already Taken.";
+			}
+					
+		}else{
+			//if no matching records
+			$response["success"] = 1;
+			$response["message"] = "Email and Username are Unique";
+			return $response;
+		}
+
+	}else{
+		//When some error occurs
+		$response["success"] = 0;
+		$response["message"] = mysqli_error($db);
+		
+	}
 
 }
 
