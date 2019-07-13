2ffd89987b200d866cc6ad0aceb98ebc9684e21d
jbialon32 <jbialon32@gmail.com>
2019-07-13 15:45:47 -0500

    Message Counter in progress Active Chat check in progress

--- app/src/main/java/net/androidbootcamp/chatterbox/messages/MessageCountRequest.java
+++ app/src/main/java/net/androidbootcamp/chatterbox/messages/MessageCountRequest.java
@@ -7,11 +7,11 @@ import com.android.volley.toolbox.StringRequest;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MessageGetRequest extends StringRequest {
-    private static final String LOGIN_REQUEST_URL = "http://192.168.1.90/api/MessageGet.php";
+public class MessageCountRequest extends StringRequest {
+    private static final String LOGIN_REQUEST_URL = "http://192.168.1.90/api/GetMessageCount.php";
     private Map<String, String> params;
 
-    public MessageGetRequest(int chatID, Response.Listener<String> listener) {
+    public MessageCountRequest(int chatID, Response.Listener<String> listener) {
         //todo Might need to change method
         super(Method.POST, LOGIN_REQUEST_URL, listener, null);
         params = new HashMap<>();
@@ -25,5 +25,4 @@ public class MessageGetRequest extends StringRequest {
     public Map<String, String> getParams() {
         return params;
     }
-
 }
